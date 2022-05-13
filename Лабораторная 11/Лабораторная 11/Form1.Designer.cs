060b7a90ad8d78c92b5350f4ca10ef972adb8153
Kozgova <91005974+Kozgova@users.noreply.github.com>
2022-05-14 00:59:12 +0700

    Add files via upload

---
+++ Лабораторная 11/Лабораторная 11/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Лабораторная_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btStart1 = new System.Windows.Forms.Button();
+            this.edMean = new System.Windows.Forms.NumericUpDown();
+            this.edVariance = new System.Windows.Forms.NumericUpDown();
+            this.edSize = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbAverage = new System.Windows.Forms.Label();
+            this.lbVarience = new System.Windows.Forms.Label();
+            this.lbSquare = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.edMean)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edVariance)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Среднее значение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Дисперсия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Размер";
+            // 
+            // btStart1
+            // 
+            this.btStart1.Location = new System.Drawing.Point(113, 241);
+            this.btStart1.Name = "btStart1";
+            this.btStart1.Size = new System.Drawing.Size(157, 89);
+            this.btStart1.TabIndex = 3;
+            this.btStart1.Text = "Сгенерировать";
+            this.btStart1.UseVisualStyleBackColor = true;
+            this.btStart1.Click += new System.EventHandler(this.btStart1_Click);
+            // 
+            // edMean
+            // 
+            this.edMean.DecimalPlaces = 2;
+            this.edMean.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.edMean.Location = new System.Drawing.Point(222, 36);
+            this.edMean.Name = "edMean";
+            this.edMean.Size = new System.Drawing.Size(120, 22);
+            this.edMean.TabIndex = 4;
+            // 
+            // edVariance
+            // 
+            this.edVariance.DecimalPlaces = 2;
+            this.edVariance.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.edVariance.Location = new System.Drawing.Point(222, 110);
+            this.edVariance.Name = "edVariance";
+            this.edVariance.Size = new System.Drawing.Size(120, 22);
+            this.edVariance.TabIndex = 5;
+            this.edVariance.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // edSize
+            // 
+            this.edSize.Location = new System.Drawing.Point(222, 183);
+            this.edSize.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.edSize.Name = "edSize";
+            this.edSize.Size = new System.Drawing.Size(120, 22);
+            this.edSize.TabIndex = 6;
+            this.edSize.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(444, 36);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(664, 516);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Среднее";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 476);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Дисперсия";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 535);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Хи квадрат";
+            // 
+            // lbAverage
+            // 
+            this.lbAverage.AutoSize = true;
+            this.lbAverage.Location = new System.Drawing.Point(209, 411);
+            this.lbAverage.Name = "lbAverage";
+            this.lbAverage.Size = new System.Drawing.Size(16, 17);
+            this.lbAverage.TabIndex = 11;
+            this.lbAverage.Text = "0";
+            // 
+            // lbVarience
+            // 
+            this.lbVarience.AutoSize = true;
+            this.lbVarience.Location = new System.Drawing.Point(209, 476);
+            this.lbVarience.Name = "lbVarience";
+            this.lbVarience.Size = new System.Drawing.Size(16, 17);
+            this.lbVarience.TabIndex = 12;
+            this.lbVarience.Text = "0";
+            // 
+            // lbSquare
+            // 
+            this.lbSquare.AutoSize = true;
+            this.lbSquare.Location = new System.Drawing.Point(209, 535);
+            this.lbSquare.Name = "lbSquare";
+            this.lbSquare.Size = new System.Drawing.Size(16, 17);
+            this.lbSquare.TabIndex = 13;
+            this.lbSquare.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 607);
+            this.Controls.Add(this.lbSquare);
+            this.Controls.Add(this.lbVarience);
+            this.Controls.Add(this.lbAverage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.edSize);
+            this.Controls.Add(this.edVariance);
+            this.Controls.Add(this.edMean);
+            this.Controls.Add(this.btStart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edMean)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edVariance)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btStart1;
+        private System.Windows.Forms.NumericUpDown edMean;
+        private System.Windows.Forms.NumericUpDown edVariance;
+        private System.Windows.Forms.NumericUpDown edSize;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbAverage;
+        private System.Windows.Forms.Label lbVarience;
+        private System.Windows.Forms.Label lbSquare;
+    }
+}
+
---
+++ Лабораторная 11/Лабораторная 11/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лабораторная_11
+{
+    public partial class Form1 : Form
+    {
+
+        double mean, variance;
+        int size;
+
+        double empMean, empVariance;
+        double errE, errD;
+        double Square;
+        double[] SquareValues = new double[15] {3.841, 5.991, 7.815, 9.488, 11.070, 12.592, 14.067, 15.507,
+                                                    16.919, 18.307, 19.675, 21.026, 22.362, 23.685, 24.996};
+
+        int k;
+        double Length;
+        int[] Count;
+        double[] Frequency;
+        double[] Len;
+        double max = -99999, min = 99999;
+        double[] normalDis = new double[10000];
+        double length;
+
+        Random rnd = new Random();
+        double l1, l2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            mean = (double)edMean.Value;
+            variance = (double)edVariance.Value;
+            size = (int)edSize.Value;
+        }
+
+        private void btStart1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            max = 0; min = 0; Square = 0;
+
+            for (int i = 0; i < size; i += 2)
+            {
+                l1 = rnd.NextDouble();
+                l2 = rnd.NextDouble();
+                normalDis[i] = Math.Sqrt((-2) * Math.Log(l1)) * Math.Sin(2 * Math.PI * l2) * Math.Sqrt(variance) + mean;
+                normalDis[i + 1] = Math.Sqrt((-2) * Math.Log(l1)) * Math.Cos(2 * Math.PI * l2) * Math.Sqrt(variance) + mean;
+                if (normalDis[i] > max)
+                    max = normalDis[i];
+                if (normalDis[i] < min)
+                    min = normalDis[i];
+                if (normalDis[i + 1] > max)
+                    max = normalDis[i + 1];
+                if (normalDis[i + 1] < min)
+                    min = normalDis[i + 1];
+
+                empMean += normalDis[i] + normalDis[i + 1];
+                empVariance += normalDis[i] * normalDis[i] + normalDis[i + 1] * normalDis[i + 1];
+            }
+
+            empMean = empMean / size;
+            empVariance = empVariance / size - empMean * empMean;
+            errE = Math.Abs(empMean - mean) / Math.Abs(mean) * 100;
+            errD = Math.Abs(empVariance - variance) / Math.Abs(variance) * 100;
+            lbAverage.Text = "" + Math.Round(empMean, 2).ToString() +
+                                "(error = " + Math.Round(errE, 2) + "%)";
+            lbVarience.Text = "" + Math.Round(empVariance, 2).ToString() +
+                                "(error = " + Math.Round(errD, 2) + "%)";
+
+            k = (int)Math.Log(size, 2) + 1;
+            Length = (max - min) / k;
+            Count = new int[k];
+            for (int i = 0; i < size; i++)
+            {
+                length = min + Length;
+                int j = 0;
+                while (j < k)
+                {
+                    if (normalDis[i] < length)
+                    {
+                        Count[j]++;
+                        break;
+                    }
+                    else
+                    {
+                        length += Length;
+                        j++;
+                    }
+                }
+                normalDis[i] = 0;
+            }
+
+            Frequency = new double[k];
+            Len = new double[k + 1];
+            double min1 = min;
+            double pi = 0, px = 0;
+            double x = 0;
+            for (int i = 0; i < k; i++)
+            {
+                Frequency[i] = (double)Count[i] / size;
+                x = (min1 + min1 + Length) / 2;
+                px = (1 / (Math.Sqrt(2 * Math.PI) * Math.Sqrt(variance))) *
+                    Math.Exp(-((x - mean) * (x - mean)) / (2 * variance));
+                pi = ((min1 + Length) - min1) * px;
+                Square += (Count[i] * Count[i]) / (double)(size * pi);
+                Len[i] = min1;
+                chart1.Series[0].Points.AddXY(min1, Frequency[i]);
+                min1 += Length;
+            }
+            chart1.Series[0].Points.AddXY(min1, Frequency[k - 1]);
+            chart1.ChartAreas[0].AxisX.Minimum = -5;
+            chart1.ChartAreas[0].AxisX.Maximum = 5;
+
+            Square -= size;
+            lbSquare.Text = "" + Math.Round(Square, 2) + " > " + SquareValues[k - 1].ToString() + "    ";
+            if (Square > SquareValues[k - 2])
+            {
+                lbSquare.Text += "True";
+            }
+            else
+                lbSquare.Text += "False";
+        }
+    }
+}
